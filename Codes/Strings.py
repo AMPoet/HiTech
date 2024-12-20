d7ca667b980d7d8957d3cb6c0ded3cd124c1bbe9
Mohammad Rahimy <134873854+AMPoet@users.noreply.github.com>
2024-12-20 17:29:31 +0330

    Strings In Python
    Define Strings In Python And How To Use Theme

---
+++ Codes/Strings.py
@@ -0,0 +1,23 @@
+#HiTech
+
+#Define A String
+string: 'This is a String •
+print(string)
+
+#Use String And Nums Together 
+num2=66.7
+sum=num1+num2
+print( Sum Of numbers equals to : , sum)
+
+#Sum Of Two Strings
+Stringl:  "First String"
+String2= "Second String "
+print (Stringl + " " + String2)
+
+
+#Follow us on social medias
+
+#Telegram: https://t.me/HiTech_Codes
+
+#Youtube:https://www.youtube.com/channel/UCBc3aSf54eyrl46RUWqIOIg?sub_confirmation=1
+
