54edda0017496dde91b89862e08f9de9c9d0e47f
Mohammad Rahimy <134873854+AMPoet@users.noreply.github.com>
2024-12-20 17:41:55 +0330

    Dictonaries and Lists
    Define And Update Dictionaries and Lists In Python
    
    You can learn this structures in more details on our YouTube Channel ( Link  in the code files and bio )

---
+++ Codes/Dictionaries and Lists.py
@@ -0,0 +1,51 @@
+# HiTech
+
+# Define a Dictionary
+My_dictionary={'name':'Mohammad','age':23 , 'job':'programmer'}
+print(My_dictionary['name'],My_dictionary['age'])
+
+# Updating_Dictionary
+
+My_dictionary={'name':'Mohammad', 'age':23 , 'job': 'programmer'}
+update_info={'city':'Kermanshah'}
+My_dictionary.update(update_info)
+print(My_dictionary)
+
+# Deleting Items From Dictionary  
+
+My_dictionary={'name':'Mohammad', 'age':23 , 'job':'programmer' , 'city':'Kermanshah'}
+del My_dictionary['age']
+print(My_dictionary)
+
+# Get Method In Dictionary
+My_dictionary={'name':'Mohammad', 'age':23 , 'job':'programmer' , 'city':'Kermanshah'}
+print(My_dictionary.get('name'))
+
+########################################
+
+# Lists
+My_List=[10,6,7,99,15,22,True,'list']
+
+# Adding Items To Lists 
+add=My_List.append(32)
+print(My_List)
+
+# Make Lists Rreverse
+list1=list(reversed(My_List))
+print(list1)
+
+
+#Follow us on social medias
+
+#Telegram: https://t.me/HiTech_Codes
+
+#Youtube:https://www.youtube.com/channel/UCBc3aSf54eyrl46RUWqIOIg?sub_confirmation=1
+
+
+
+
+
+
+
+
+
