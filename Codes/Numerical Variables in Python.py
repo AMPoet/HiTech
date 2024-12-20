638fd353480be2aa32fcb7d65b85287b897c0e65
Mohammad Rahimy <134873854+AMPoet@users.noreply.github.com>
2024-12-20 17:05:44 +0330

    Numerical Variables
    How to define numerical variables in python

---
+++ Codes/Numerical Variables in Python.py
@@ -0,0 +1,47 @@
+#HiTech
+
+# Numerical Variables in Python
+
+x=12.99
+print(type(x)) #output: int
+
+############################
+
+# Adding two numbers together
+x = 3.98               # declearing First variable(float)
+y = 5.67               # declearing Second variable(float)
+sum= x+y               # declearing Third variable for calculating 
+print(sum)             # output: 9.65 
+
+############################
+
+# Adding two numbers together (other way)
+x = 3.98               # declearing First variable(float)
+y = 5.67               # declearing Second variable(float)
+print(x+y)             # output: 9.65 
+
+############################     
+
+# Adding a float number to an integer number
+x = 16                 # declearing First variable(int)
+y = 120.5              # declearing Second variable(float)
+sum = float(x)+ y      # converting the type of x into float
+print(sum)             # output: 136.5
+
+############################
+
+# Adding a float number to an integer number (other way)
+x = 16                 # declearing First variable(int)
+y = 120.5              # declearing Second variable(float)
+sum = int(y)+ x        # converting the type of y into integer
+print(sum)             # output: 136.5
+
+
+#Follow us on social medias
+
+#Telegram: https://t.me/HiTech_Codes
+
+#Youtube:https://www.youtube.com/channel/UCBc3aSf54eyrl46RUWqIOIg?sub_confirmation=1
+
+
+
