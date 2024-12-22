14486bbc1c1c8b7ebaab0cf2722f8c21ec97811d
Mohammad Rahimy <134873854+AMPoet@users.noreply.github.com>
2024-12-22 21:29:12 +0330

    How to make a Tuple in Python
    for more description follow the links in code file and watch the YouTube video✅🧑🏻‍💻

---
+++ Codes/Tuple.py
@@ -0,0 +1,26 @@
+#HiTech
+
+My_tuple=(0,1,2,3,4,5,6,7,8,9,10,9)   #Define A Tuple
+print(My_tuple)
+
+
+My_tuple=(0,1,2,3,4,5,6,7,8,9,10,9)
+length=len(My_tuple)                  #Calculate the length of Tuple
+print("The length is : ", length)    #print the length 
+
+
+My_tuple=(0,1,2,3,4,5,6,7,8,9,10,9)
+for elements in My_tuple:             #Define a Loop
+   print(elements)            
+
+My_tuple=(0,1,2,3,4,5,6,7,8,9,10,9)
+index= My_tuple.index(7)              #Calculate the index of number 7
+print(index)
+
+My_tuple=(0,1,2,3,4,5,6,7,8,9,10,9)
+elements=My_tuple[1:6]                #Select The indexe 1 to 6 of tuple
+print(elements) 
+
+#Telegram: https://t.me/HiTech_Codes
+
+#Youtube:https://www.youtube.com/channel/UCBc3aSf54eyrl46RUWqIOIg?sub_confirmation=1
